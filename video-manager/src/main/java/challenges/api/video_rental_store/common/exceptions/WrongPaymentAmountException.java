95206d2e476f8f7ca4d7760b4fb262fc8e549145
Carlos de la Fuente <carlos.delafuente@erni-espana.es>
2020-04-22 16:20:53 +0200

    FIX exception message

--- video-manager/src/main/java/challenges/api/video_rental_store/common/exceptions/WrongPaymentAmountException.java
+++ video-manager/src/main/java/challenges/api/video_rental_store/common/exceptions/WrongPaymentAmountException.java
@@ -3,7 +3,7 @@ package challenges.api.video_rental_store.common.exceptions;
 public class WrongPaymentAmountException extends RuntimeException {
 
 	public WrongPaymentAmountException(int price) {
-		super("The payment amount must be higher than the price: " + price);
+		super("The payment amount must be equal to the price: " + price);
 	}
 
 }
